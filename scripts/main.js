b0f20d1142fad87ceb0db4276c8de92ada914429
mutembeijoe <mutembeijoe@gmail.com>
2019-08-30 08:14:13 +0300

    define the main Pizza constructor

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+const Pizza = function(name,size){
+    this.name = name;
+    this.size = size;
+    Object.defineProperty(this,'price',{
+        get: function(){
+            switch (this.size) {
+                case 'small': return this.listPrice[0]
+                    break;
+                case 'medium': return this.listPrice[1]
+                    break;
+                case 'large':return this.listPrice[2]
+                default: alert('error')
+                    break;
+            }
+        }
+    })
+}
+
+
+// const myPizza = new Pizza('BigMark','small');
+// console.log(myPizza);
+
+
+
+
+
+
 $(function(){
 
 
