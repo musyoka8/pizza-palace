04214ef367caaca2f1a23a227c0a1b83d64013dc
mutembeijoe <mutembeijoe@gmail.com>
2019-08-30 09:08:31 +0300

    add a method to get total price of a pizza to the main pizza constructor

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 const Pizza = function(name,size){
     this.name = name;
     this.size = size;
-    this.topping = null;
-    this.crust = null;
+    this.topping = 'mushrooms';
+    this.crust = 'puff';
     Object.defineProperty(this,'price',{
         get: function(){
             switch (this.size) {
@@ -18,27 +18,57 @@ const Pizza = function(name,size){
     })
 }
 
+Pizza.prototype.getTotalPrice = function(){
+    let toppingPrice;
+    let crustPrice;
+    switch (this.topping) {
+        case 'pepperoni': toppingPrice = 100;
+            break;
+        case 'mushrooms': toppingPrice = 200;
+            break;
+        case 'onions' : toppingPrice = 80;
+            break;
+        case 'bacon' : toppingPrice = 150;
+            break;
+        default: toppingPrice = 0;
+            break;
+    }
+
+    switch (this.crust) {
+        case 'glutenFree': crustPrice = 300;
+            break;
+        case 'puff': crustPrice = 170;
+            break;
+        case 'crispy' : crustPrice = 200;
+            break;
+        case 'classic' : crustPrice = 10;
+            break;
+        default: crustPrice= 0;
+            break;
+    }
+    
+
+    return toppingPrice + crustPrice + this.price;
+   
+}
 
 const BigMark = function(name,size){
     Pizza.call(this, name, size);
     this.listPrice = [500,750,1000]
 }
+BigMark.prototype = new Pizza();
 
 const HeavyJoe = function(name,size){
     Pizza.call(this,name,size);
     this.listPrice = [400,600,1200]
 }
+HeavyJoe.prototype = new Pizza();
 
 
 
 
 const myPizza = new HeavyJoe('BigMark','small');
 console.log(myPizza);
-
-
-
-
-
 
 $(function(){
 
